687d0f1dcefa2bc3ce01b834f21d7c161bdc2068
Baptiste Reveyron <baptiste@rvrn.fr>
2021-10-02 20:23:42 +0200

    Désactive la récupération des credentials locaux si les variables d'environnement ahdoc sont fournies (dirty)

--- src/services/S3/index.ts
+++ src/services/S3/index.ts
@@ -1,8 +1,6 @@
 import AWS from "aws-sdk";
 import cleanDeep from "clean-deep";
 
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-
 import type IS3Service from "./type";
 
 class S3Service implements IS3Service 
@@ -10,14 +8,21 @@ class S3Service implements IS3Service
     _bucket : string
     _s3 : AWS.S3
 
-    constructor(bucket: string, region: string) {
-        
+    constructor(bucket: string, region: string) 
+    {
+        // Dirty : if the credentials environment variables are not provided (lambda aws environment), 
+        // try to access a local credential file (local node environment)
+        if (!process.env.AWS_ACCESS_KEY_ID && !process.env.AWS_SECRET_ACCESS_KEY)
+        {
+            var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+            AWS.config.credentials = credentials;
+        }
+
         AWS.config.update({region: region});
-        AWS.config.credentials = credentials;
         this._s3 = new AWS.S3();
         this._bucket = bucket;
     }
-
+    
     /**
      * @returns URL to get the content
      */
