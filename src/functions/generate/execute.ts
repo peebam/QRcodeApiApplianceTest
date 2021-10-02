ee5fa237bb60eda114daddfb4f6448d65b12c265
Baptiste Reveyron <baptiste@rvrn.fr>
2021-10-02 09:45:26 +0200

    Génération des n qrcodes avec un id générarer aléatoirement

--- src/functions/generate/execute.ts
+++ src/functions/generate/execute.ts
@@ -1,34 +1,45 @@
 import qrCode from "@services/qrcodes";
 import S3 from "@services/s3";
+import { customAlphabet } from "nanoid";
 
 const bucket : string = "br-bucket-dev";
 const region : string = "us-east-2";
 const s3 : S3 = new S3(bucket, region);
 
-function buildS3DocumentPath(id : string, prefix?  : string)
-{
-    
-}
+const s3Root = "qrcodes"
+const idAlphabet = "ABCDEFGHJKLMNPQRSTUVWXYZ0123456789"
 
-function generateContent(prefix?  : string) : string
-{
-    return "content";
-}
+const buildS3DocumentPath = (id: string) : string => `${s3Root}/${id}`;
+
+const generateId : () => string = customAlphabet(idAlphabet, 7);
+
+const generateContent = (id: string, prefix?  : string) : string => `${prefix}${id}`;
 
 async function generateQrcode(id : string, prefix? : string) : Promise<string>
 {
     let content = generateContent(prefix);
     let buffer : Buffer = await qrCode.generate(content);
-    return await s3.upload("test", buffer, "png");
+
+    let path : string = buildS3DocumentPath(id);
+    let metadata : Record<string, string> = {
+        "id": id,
+        "prefix" : prefix
+    };
+    
+    return await s3.upload(path, buffer, "png", metadata);
 }
 
 export default async function (number: number, prefix? : string) : Promise<Record<string, string>>
 {
     let locations = {};
+    
+    for(let i = 0; i < number; i++)
+    {
+        let id = generateId();
 
-    let id = "zozo";
-    let location = await generateQrcode(id, prefix);
+        let location = await generateQrcode(id, prefix);
+        locations[id] = location;
+    }
 
-    locations[id] = location;
     return locations;
 };
